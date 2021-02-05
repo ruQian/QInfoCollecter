c9a2bb4a474548979c0962d2ca2cb7543ac1bcd7
rubo <rubo@e-u.cn>
2021-02-05 14:25:45 +0800

    update

--- src/QUtil.cpp
+++ src/QUtil.cpp
@@ -1,5 +1,6 @@
 #include "QUtil.h"
-#include <windows.h>
+
+#include <mirai.h>
 // UTF8转std:string
 // 转换过程：先将utf8转双字节Unicode编码，再通过WideCharToMultiByte将宽字符转换为多字节。
 std::string UTF8_To_string(const std::string& str) 
